3f1365f93b43dfe9a87d29c4187e47479f747b96
kjhbbjoker <whdgns4288@naver.com>
2022-01-25 08:48:00 +0900

    게시판
    일단 백엔드 작성한 날짜대로 조회 되는거 말고는 다 구현

--- src/main/java/com/example/ProjectBoard/repository/BoardRepository.java
+++ src/main/java/com/example/ProjectBoard/repository/BoardRepository.java
@@ -4,4 +4,5 @@ import com.example.ProjectBoard.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Integer> {
+
 }
